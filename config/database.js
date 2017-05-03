11e1afa87c1ba7edca01715e6f1e9f6c614dc234
stav <lastav5@gmail.com>
2017-05-03 22:33:01 +0300

    fixed database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
-var url;
+var details,url;
 try {
-    url = require('../config/herokuDatabase.js');
+    details = require('../config/herokuDatabase.js');
 }
 catch (err) {}
 
 module.exports = {
-    url: process.env.HEROKU_DB_CONNECTION_STRING || url // local db: process.env.LOCAL_DB_CONNECTION_STRING
+    url: process.env.HEROKU_DB_CONNECTION_STRING || details.url // local db: process.env.LOCAL_DB_CONNECTION_STRING
 };
