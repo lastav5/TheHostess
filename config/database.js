ccde51211613d3f0dd6820461554b119dd63623c
stav <lastav5@gmail.com>
2017-05-03 22:45:50 +0300

    fixed database.js

--- config/database.js
+++ config/database.js
@@ -5,5 +5,5 @@ try {
 catch (err) {}
 
 module.exports = {
-    url: process.env.HEROKU_DB_CONNECTION_STRING || details.url // local db: process.env.LOCAL_DB_CONNECTION_STRING
+    url: process.env.DATABASE_URL || details.url /* local db: process.env.LOCAL_DB_CONNECTION_STRING*/
 };
